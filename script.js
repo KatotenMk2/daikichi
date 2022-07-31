e79e6babfada113cf4468bed0e1c7ff6a981f8a9
Mitchy <106158780+mitchy5426@users.noreply.github.com>
2022-07-31 16:16:40 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+$(".faq__q").on("click", function () {
+    $(this).parent().toggleClass("-active");
+    $(this).next().slideToggle();
+  });
+
+
+  /* --------------------------------------- */
+/* iOS用フルスクリーンのテコ入れ */
+/* --------------------------------------- */
+const setFillHeight = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty("--vh", `${vh}px`);
+};
+
+let vw = window.innerWidth;
+
+window.addEventListener("resize", () => {
+  if (vw === window.innerWidth) {
+    // 画面の横幅にサイズ変動がないので処理を終える。ガタツキ防止
+    return;
+  }
+
+  // 画面の横幅のサイズ変動があった時のみ高さを再計算する
+  vw = window.innerWidth;
+  setFillHeight();
+});
+
+// 初期化
+setFillHeight();
